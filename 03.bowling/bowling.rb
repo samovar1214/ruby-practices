928645b569c6481c18cabd998a5679ab61f9871d
samovar <samovar1214@gmail.com>
2025-05-08 21:29:38 +0900

    変数名とpointの計算を修正
    1球毎に分割し数字に変換するまでの処理を短い書き方に、フレーム毎に分割する部分はframes = []を省略できる書き方に修正しました。また、ブロックの引数の変数名を1文字の名前から分かりやすい名前に修正しました。
    
    pointの計算はEnumerable#sumを使う形に変更しました。frames[n].sumの重複を解消し、1～8フレーム目と9フレーム目の処理をまとめました。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,53 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
-scores = score.split(',')
+shots = ARGV[0].split(',').map { |s| s == 'X' ? 10 : s.to_i }
 
-shots = []
-scores.each do |s|
-  shots <<  if s == 'X'
-              10
-            else
-              s.to_i
-            end
-end
-
-frames = []
 one_frame = []
-shots.each do |s|
-  one_frame << s
+frames = shots.each_with_object([]) do |shot, frames|
+  one_frame << shot
   if frames.count < 10
-    if one_frame.count == 2 || s == 10
+    if one_frame.count == 2 || shot == 10
       frames << one_frame.clone
       one_frame.clear
     end
   else
-    frames[-1] << s
+    frames[-1] << shot
   end
 end
 
-point = 0
-(0..7).each do |n|
-  two_frames = frames[n + 1] + frames[n + 2]
-  point +=
-    if frames[n][0] == 10
-      10 + two_frames[0] + two_frames[1]
-    elsif frames[n].sum == 10
-      10 + frames[n + 1][0]
-    else
-      frames[n].sum
-    end
+point = (0..8).each.sum do |index|
+  bonus = if frames[index][0] == 10
+            frames[index + 1] ||= []
+            frames[index + 2] ||= []
+            two_frames = frames[index + 1] + frames[index + 2]
+            two_frames[0..1].sum
+          elsif frames[index].sum == 10
+            frames[index + 1][0]
+          else
+            0
+          end
+  frames[index].sum + bonus
 end
-
-point +=
-  if frames[8][0] == 10
-    10 + (frames[9][0] + frames[9][1])
-  elsif frames[8].sum == 10
-    10 + frames[9][0]
-  else
-    frames[8].sum
-  end
 
 point += frames[9].sum
 puts point
