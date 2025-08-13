efc12a9164132862fcf3e25e68cc602e6956733b
samovar <samovar1214@gmail.com>
2025-08-13 21:30:08 +0900

    lsコマンド2を作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
 def layout_by_columns(filenames, column_count)
-  row_count = (filenames.length.to_f / column_count).ceil
+  row_count = filenames.length.ceildiv(column_count)
   rows = Array.new(row_count) { [] }
 
   filenames.each_with_index do |filename, index|
@@ -24,7 +26,10 @@ def print_rows(rows)
   end
 end
 
-filenames = Dir.glob('*').sort
+options = ARGV.getopts('a')
+
+filenames = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
+
 column_count = 3
 rows = layout_by_columns(filenames, column_count)
 print_rows(rows)
